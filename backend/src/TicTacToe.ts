5f17809d43d4b913f96dbe5aa2d2b9be0534c348
Mason Lane <github@masonlane.me>
2025-04-01 06:11:16 -0500

    Consolidate TicTacToe.ts and TicTacToeTypes.ts

--- backend/src/TicTacToe.ts
+++ backend/src/TicTacToe.ts
@@ -1,4 +1,17 @@
-import { TicTacToeGame, TicTacToeBoard } from './TicTacToeTypes'
+export type TicTacToeBoard = Array<null | "X" | "O">
+
+export interface TicTacToeGame {
+    board: TicTacToeBoard;
+    turnNumber: number;
+    currentPlayer: "X" | "O";
+    xID: string | null;
+    oID: string | null;
+    gameState: string;
+    checkBoard(board: TicTacToeBoard): string;
+    advanceTurn(): void;
+    restart(): void;
+    handleClick(object: { index: number, lobbyID: string, player: "X" | "O", }): boolean;
+}
 
 export default class tttGame implements TicTacToeGame {
     board: TicTacToeBoard
