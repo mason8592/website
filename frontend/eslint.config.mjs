349dc77c3503a43b9a8cecc20dc51a2e44fc7649
Mason Lane <github@masonlane.me>
2025-04-03 11:10:51 -0500

    Optimize frontend eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,43 +4,32 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-export default tseslint.config(
-  { ignores: ['dist', 'node_modules'] },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      'plugin:@typescript-eslint/strict-type-checked',
-      'plugin:react/recommended',
-      'plugin:react/jsx-runtime',
-    ],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: ['./tsconfig.json', './tsconfig.node.json'],
-        tsconfigRootDir: __dirname,
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+export default tseslint.config({
+  ignores: ['dist', 'node_modules'],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  languageOptions: {
+    globals: globals.browser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      project: ['./tsconfig.json'],
+      tsconfigRootDir: "./",
     },
   },
-)
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+})
