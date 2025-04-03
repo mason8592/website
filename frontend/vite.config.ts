108184703fd1ec266ca203928d3718ede96073ce
Mason Lane <github@masonlane.me>
2025-04-03 14:14:13 -0500

    Optimize vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const outDir = path.resolve(__dirname, "../dist/frontend")
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     target: "esnext",
+    outDir: outDir
   },
-  server: {
-    // port: 3000,
-    // host: '0.0.0.0',
-    // proxy: {
-    //     "/socket.io/": {
-    //         target: "http://localhost:4000",
-    //         changeOrigin: true,
-    //         secure: false,
-    //         ws: true
-    //     }
-    // }
+  resolve: {
+    alias: {
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+    }
   }
 })
